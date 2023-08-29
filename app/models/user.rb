36561d8b63ee45f2c00b1b6a1f38e00af4b6df87
jcartronics <jcar-95@hotmail.com>
2023-08-28 23:46:04 -0400

    Feat:(User views) Vistas generadas y modificacion en el modelo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Muestra en la vista uno por defecto
+  def full_name
+    name.blank? ? email : name
+  end
 end
