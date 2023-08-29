e530c49125ce781168c744138f54f1a960402b80
jcartronics <jcar-95@hotmail.com>
2023-08-28 23:30:05 -0400

    Feat:(User) Configuracion inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :contacts, only: [:new, :create]
+  root "pages#home"
+
+  #Devise
+  devise_for :users, controllers: { registrations: 'registrations' },
+                      path: '',
+                      path_names: { sign_in: 'login',
+                                    sign_out: 'logout',
+                                    sign_up: 'registrate' }
+
+  # Vistas estaticas
   get '/home', to: 'pages#home'
   get '/legal', to: 'pages#terms'
   get '/privacy', to: 'pages#privacy'
-  root "pages#home"
+  resources :contacts, only: [:new, :create]
 end
